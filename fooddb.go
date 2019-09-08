05532741af68618466f61b134d89d97ed940738f
Taisuke Fukuno <fukuno@jig.jp>
2019-09-08 19:45:34 +0900

    shorten code to add record

--- fooddb.go
+++ fooddb.go
@@ -34,8 +34,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var db *gorm.DB
-
 type Food struct {
 	ID        int       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -44,25 +42,23 @@ type Food struct {
 	URL       string    `json:"url"`
 }
 
+var db *gorm.DB
+
 func InitDB() {
 	USER := os.Getenv("MYSQL_USER")
 	PASS := os.Getenv("MYSQL_PASSWORD")
 	PROTOCOL := ""
 	DBNAME := os.Getenv("MYSQL_DATABASE")
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
 
 	var err error
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
-	fmt.Println(CONNECT)
-
 	db, err = gorm.Open("mysql", CONNECT) // github.com/go-sql-driver/mysql
 	if err != nil {
 		panic(err.Error())
 	}
-
 	db.LogMode(true)
 	db.AutoMigrate(&Food{})
 }
-
 func TruncateTables() {
 	rows, err := db.Raw("SHOW TABLES").Rows()
 	if err != nil {
@@ -77,72 +73,44 @@ func TruncateTables() {
 		db.Exec("TRUNCATE TABLE " + table)
 	}
 }
-
 func GetDB() (*gorm.DB, error) {
 	if db == nil {
 		return nil, errors.New("can't get database")
 	}
 	return db, nil
 }
-
 func Finalize() error {
 	err := db.Close()
 	return err
 }
-
-func ReadAllFoods() {
+func ShowAllFoods() {
 	db, err := GetDB()
 	if err != nil {
-		fmt.Println("err")
+		fmt.Println(err)
 		return
 	}
 	var foods []Food
 	db.Table("Foods").Find(&foods)
 	fmt.Println(foods)
 }
-
-func AddFood(name string, price int, url string) {
-	param := Food{ Name: name, Price: price, URL: url, CreatedAt: time.Now() }
+func AddFood(name string, price int, url string) error {
 	db, err := GetDB()
 	if err != nil {
-		return
+		return err
 	}
-	/* // 日付設定する場合
-	timeformat := "2006-01-02 15:04:05"
-	ct, err := time.Parse(timeformat, "2019-09-04 04:35:05")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	*/
-	tx := db.Table("Foods").Begin()
-	tx.Create(&param)
-	if tx.Error != nil {
-		fmt.Println("starting transition failed.")
-		tx.Rollback()
-		return
-	}
-	if len(db.GetErrors()) != 0 {
-		fmt.Println("changing database failed.")
-		tx.Rollback()
-		return
-	}
-	tx.Commit()
+	db.Create(&Food{ Name: name, Price: price, URL: url, CreatedAt: time.Now() })
+	return nil
 }
-
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println("Error loading .env file")
 		return
 	}
-
 	InitDB()
 	TruncateTables()
-	
 	AddFood("サラダ焼", 120, "https://www.sanrokusyoten-316.co.jp/products.html")
 	AddFood("サバエドッグ", 280, "https://www.meat-sasaki.com")
 	AddFood("眼鏡堅麺麭", 864, "http://echizen-yumekobo.com/?pid=52337987")
-
-	ReadAllFoods()
+	ShowAllFoods()
 }
